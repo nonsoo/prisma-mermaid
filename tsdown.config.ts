a37c1d413200f8b1d1581d27c122a3a3f13cfeb3
Nonso <nonsootoh@gmail.com>
2026-09-12 23:01:37 -0400

    chore: replace tsup with tsdown for build process and add tsdown configuration

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/lib/PrismaMermaidGenerators/bin.ts"],
   outDir: "build",
-  splitting: false,
-  treeshake: false,
   dts: true,
   clean: true,
   platform: "node",
   target: "node22",
   format: ["esm", "cjs"],
-  esbuildOptions: (options) => {
-    options.alias = {
-      "@": "./src",
-    };
+  alias: {
+    "@": "./src",
   },
 });
